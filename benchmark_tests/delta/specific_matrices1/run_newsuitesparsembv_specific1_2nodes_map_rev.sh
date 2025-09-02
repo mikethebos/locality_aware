bb07abce406e5e0a0ce4be643f0c11019d47f328
Mike Adams <mikethebos@icloud.com>
2025-09-01 21:52:01 -0600

    run matrices individually

--- benchmark_tests/delta/specific_matrices1/run_newsuitesparsembv_specific1_2nodes_map_rev.sh
+++ benchmark_tests/delta/specific_matrices1/run_newsuitesparsembv_specific1_2nodes_map_rev.sh
@@ -21,24 +21,34 @@ SPECIFIC_MATS="$HOME/test_matrices/CurlCurl_4.pm $HOME/test_matrices/nlpkkt80.pm
 echo "Running Unthreaded Neighbor_Alltoallv Test:"
 echo "Running Unthreaded Neighbor_Alltoallv Test:" 1>&2
 export OMP_NUM_THREADS=1
-mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_neighbor_alltoallv_matblockvec r $SPECIFIC_MATS
+for MAT in $SPECIFIC_MATS; do
+    mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_neighbor_alltoallv_matblockvec r $MAT
+done
 
 echo "Running Threaded Neighbor_Alltoallv Test with 2 threads:"
 echo "Running Threaded Neighbor_Alltoallv Test with 2 threads:" 1>&2
 export OMP_NUM_THREADS=2
-mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $SPECIFIC_MATS
+for MAT in $SPECIFIC_MATS; do
+    mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $MAT
+done
 
 echo "Running Threaded Neighbor_Alltoallv Test with 4 threads:"
 echo "Running Threaded Neighbor_Alltoallv Test with 4 threads:" 1>&2
 export OMP_NUM_THREADS=4
-mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $SPECIFIC_MATS
+for MAT in $SPECIFIC_MATS; do
+    mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $MAT
+done
 
 echo "Running Threaded Neighbor_Alltoallv Test with 8 threads:"
 echo "Running Threaded Neighbor_Alltoallv Test with 8 threads:" 1>&2
 export OMP_NUM_THREADS=8
-mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $SPECIFIC_MATS
+for MAT in $SPECIFIC_MATS; do
+    mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $MAT
+done
 
 echo "Running Threaded Neighbor_Alltoallv Test with 16 threads:"
 echo "Running Threaded Neighbor_Alltoallv Test with 16 threads:" 1>&2
 export OMP_NUM_THREADS=16
-mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $SPECIFIC_MATS
+for MAT in $SPECIFIC_MATS; do
+    mpirun --map-by ppr:1:numa --bind-to numa --rank-by slot --display-map --display-allocation --report-bindings ./new_gpuaware_gpu_suitesparse_threaded_neighbor_alltoallv_matblockvec r $MAT
+done
